e382bb0690bd11171e176e44133cc3d09ffb3623
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-05-26 16:00:38 +0200

    Add sos and eos to sentences.
    Co-authored-by: FloydTheBarber94 <FloydTheBarber94@users.noreply.github.com>

--- Data/data_preparation.py
+++ Data/data_preparation.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import random
+import csv
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 import torch
@@ -11,7 +13,7 @@ def create_dataframe(filename):
     if 'Unnamed: 0' in df.columns:
         df = df.drop(columns=['Unnamed: 0'])
     
-    # Concatenate questions and answers vertically 
+    # Concatenate questions and answers vertically (for creating train and val data)
     stacked_df = pd.concat([df['question'], df['answer']]).dropna().reset_index(drop=True)
     
     return stacked_df
@@ -26,14 +28,30 @@ def get_sentence_tokens(filename):
     Returns:
         sentence_tokens (list): list of lists of tokens (list of sentences).
     """
-    conv_data = create_dataframe(filename)
-    sentence_tokens = conv_data.apply(word_tokenize)     # Lists of tokens
-    
-    return sentence_tokens.tolist()
+    sentence_tokens = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row: 
+                sentence = row[0]
+                tokens = word_tokenize(sentence)
+                sentence_tokens.append(['<sos>'] + tokens + ['<eos>'])
+    return sentence_tokens
 
 
-def word_index_mappings():
-    pass
+def word_index_mappings(file_path ='Data/Datasets/conv_train.csv'):
+    train_sentences = get_sentence_tokens(file_path)
+    id2tok = ['<unk>', '<pad>']
+    tok2id = {'<unk>': 0, '<pad>': 1}
+    max_sentence_length = 0
+    for s in train_sentences:
+        max_sentence_length = max(max_sentence_length, len(s))
+        for t in s:
+            if t not in tok2id:
+                tok2id[t] = len(id2tok)
+                id2tok.append(t)
+
+    return tok2id, id2tok
 
 
 def read_embeddings(pth, vocab, dim):
@@ -50,6 +68,27 @@ def read_embeddings(pth, vocab, dim):
         print(f'Found embeddings for {found_tokens} / {len(vocab)}')
     return torch.tensor(emb, dtype=torch.float32)
 
+def write_sentences(path, data):
+        with open(path, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            for sentence in data:
+                writer.writerow([''.join(sentence)])
+
+def train_val_split(filename, train_ratio=0.95, seed=42, train_out="conv_train.csv", val_out="conv_val.csv"):
+    sentences = create_dataframe(filename)
+            
+    random.seed(seed)
+    random.shuffle(sentences)
+
+    split_idx = int(len(sentences) * train_ratio)
+    train_sentences = sentences[:split_idx]
+    val_sentences = sentences[split_idx:]
+
+    write_sentences(train_out, train_sentences)
+    write_sentences(val_out, val_sentences)
+
+    print(f"Train size: {len(train_sentences)}")
+    print(f"Validation size: {len(val_sentences)}")
     
 class FullSentenceDataset(Dataset):
     """
@@ -83,10 +122,13 @@ def collate_full_sentences(batch):
     return x_padded, y_padded
 
 if __name__== "__main__":
-    sentence_tokens = get_sentence_tokens("Data/Datasets/Conversation.csv")
+    sentence_tokens = get_sentence_tokens("Data/Datasets/conv_train.csv")
     
     print(len(sentence_tokens))
     print(sentence_tokens[1000])
+    
+    #train_val_split("Data/Datasets/Conversation.csv", train_ratio=0.95, seed=42, train_out="Data/Datasets/conv_train.csv", val_out="Data/Datasets/conv_val.csv")
+    
 
     
     
